aaa55d811bd861d8f9fb8eef88ed83d8cd389a13
Radovan Semancik <radovan.semancik@evolveum.com>
2020-10-06 18:17:13 +0200

    build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+VERSION=0.1.0
+
+cd evolveum-jekyll-plugin
+gem build evolveum-jekyll-plugin.gemspec
+gem install evolveum-jekyll-plugin-0.1.0.gem
+cd ..
+
+cd evolveum-jekyll-theme
+gem build evolveum-jekyll-theme.gemspec
+gem install evolveum-jekyll-theme-$VERSION.gem
+cd ..
