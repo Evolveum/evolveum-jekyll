a6e9e213dde40e48cf28b5efeed1455f6f41630a
Radovan Semancik <radovan.semancik@evolveum.com>
2021-02-17 16:34:13 +0100

    Google-pure sitemap and custom searchmap

--- evolveum-jekyll-plugin/lib/evolveum/jekyll-sitemap-plugin.rb
+++ evolveum-jekyll-plugin/lib/evolveum/jekyll-sitemap-plugin.rb
@@ -1,10 +1,11 @@
-# (C) 2020 Evolveum
+# (C) 2021 Evolveum
 #
 # Evolveum page Plugin for Jekyll
 #
 # This plugin is responsible for generating:
-#  page.html: user-friendly, readable, hierarchical HTML page
-#  page.xml: flat XML page for Google and other robots
+#  sitemap.html: user-friendly, readable, hierarchical HTML page
+#  sitemap.xml: flat XML page for Google and other robots
+#  searchmap.xml: flat XML for our custom quicksearch
 #  robots.txt
 #
 # NOTE: This has to be a plugin, not just a couple of Liquid files, as Jekyll has this idealistic strict separation of design and content.
@@ -15,11 +16,17 @@ module Evolveum
     class SiteMapGenerator < Jekyll::Generator
         priority :lowest
 
+        FILENAME_SITEMAP_XML = 'sitemap.xml'
+        FILENAME_SITEMAP_HTML = 'sitemap.html'
+        FILENAME_SEARCHMAP = 'searchmap.xml'
+        FILENAME_ROBOTS = 'robots.txt'
+
         def generate(site)
             @site = site
-            @site.pages << generateSiteMapXml() unless pageExists?("sitemap.xml")
-            @site.pages << generateSiteMapHtml() unless pageExists?("sitemap.html")
-            @site.pages << generateRobotsTxt() unless pageExists?("robots.txt")
+            @site.pages << generateSiteMapXml() unless pageExists?(FILENAME_SITEMAP_XML)
+            @site.pages << generateSiteMapHtml() unless pageExists?(FILENAME_SITEMAP_HTML)
+            @site.pages << generateSearchMap() unless pageExists?(FILENAME_SEARCHMAP)
+            @site.pages << generateRobots() unless pageExists?(FILENAME_ROBOTS)
         end
 
         private
@@ -36,25 +43,34 @@ module Evolveum
             end
 
             def generateSiteMapXml()
-              page = Jekyll::PageWithoutAFile.new(@site, __dir__, "", "sitemap.xml")
-              page.content = File.read(sourceFilePath("sitemap.xml")).gsub(MINIFY_REGEX, "")
+              page = Jekyll::PageWithoutAFile.new(@site, __dir__, "", FILENAME_SITEMAP_XML)
+              page.content = File.read(sourceFilePath(FILENAME_SITEMAP_XML)).gsub(MINIFY_REGEX, "")
               page.data["layout"] = nil
               page.data["visibility"] = "system"
               page
             end
 
+            def generateSearchMap()
+              page = Jekyll::PageWithoutAFile.new(@site, __dir__, "", FILENAME_SEARCHMAP)
+              page.content = File.read(sourceFilePath(FILENAME_SEARCHMAP)) # No minification here
+              page.data["layout"] = nil
+              page.data["visibility"] = "system"
+              page
+            end
+
+
             def generateSiteMapHtml()
-              page = Jekyll::PageWithoutAFile.new(@site, __dir__, "", "sitemap.html")
-              page.content = File.read(sourceFilePath("sitemap.html")) # No minification here
+              page = Jekyll::PageWithoutAFile.new(@site, __dir__, "", FILENAME_SITEMAP_HTML)
+              page.content = File.read(sourceFilePath(FILENAME_SITEMAP_HTML)) # No minification here
               page.data["layout"] = "page"
               page.data["visibility"] = "auxiliary"
               page.data["title"] = "Site Map"
               page
             end
 
-            def generateRobotsTxt()
-              page = Jekyll::PageWithoutAFile.new(@site, __dir__, "", "robots.txt")
-              page.content = File.read(sourceFilePath("robots.txt"))
+            def generateRobots()
+              page = Jekyll::PageWithoutAFile.new(@site, __dir__, "", FILENAME_ROBOTS)
+              page.content = File.read(sourceFilePath(FILENAME_ROBOTS))
               page.data["layout"] = nil
               page.data["visibility"] = "system"
               page
