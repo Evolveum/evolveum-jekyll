1e88ab416b97160df4e81318aaf8be00937a934f
jmederly <jan.mederly@evolveum.com>
2021-02-23 17:42:17 +0100

    Test no.7 minor design changes

--- evolveum-jekyll-theme/assets/js/evolveum/search.js
+++ evolveum-jekyll-theme/assets/js/evolveum/search.js
@@ -81,7 +81,7 @@ function searchForPhrase() {
 
                     const listItem = '<a href="' + searchMap[i].url + '">' +
                         '<li class="list-group-item border-0" style="width: 100%"><i class="fas fa-align-left"></i><span class="font1">' + ' &nbsp; ' + searchMap[i].title + '<br></span>' +
-                        '<span class="font2">' + searchMap[i].preview.substring(0, 50); + '</span></li></a>';
+                        '<span class="font2">' + searchMap[i].preview.substring(0, 85) + '</span></li></a>';
                     if (title.localeCompare(phrase) === 0) {
                         console.log('input is title')
                         showItemsTitleMatch.push(listItem)
