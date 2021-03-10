9d89425812617c760bf2333131713dc0c5d289c5
Radovan Semancik <radovan.semancik@evolveum.com>
2021-03-10 13:44:51 +0100

    local redirects

---
+++ evolveum-jekyll-plugin/lib/evolveum/jekyll-redirect-plugin.rb
@@ -0,0 +1,49 @@
+# (C) 2021 Evolveum
+#
+# Evolveum page Plugin for Jekyll
+#
+# TODO
+#
+# NOTE: This has to be a plugin, not just a couple of Liquid files, as Jekyll has this idealistic strict separation of design and content.
+
+
+module Evolveum
+
+    class HtaccessGenerator < Generator
+        priority :lowest
+
+        FILENAME = '.htaccess'
+
+        def generate(site)
+            @site = site
+            @nav = site.data['nav']
+
+            page = Jekyll::PageWithoutAFile.new(@site, __dir__, "", FILENAME)
+            page.content = File.read(sourceFilePath(FILENAME))
+            page.data["layout"] = nil
+            page.data["visibility"] = "system"
+            page.data["redirects"] = collectRedirects()
+
+            @site.pages << page
+        end
+
+        def collectRedirects()
+            redirects = []
+            @site.pages.each do |page|
+                if page.data['moved-from']
+                    Array(page.data['moved-from']).each do |movedFrom|
+                        # We do not want to start pattern with /
+                        # This is .htaccess, paths are relative to the directory in which .htaccess is
+                        redirects << { "pattern" => "^" + escapePattern(movedFrom) + "$",  "substitution" => page.url }
+                    end
+                end
+            end
+            return redirects
+        end
+
+        def escapePattern(orig)
+            orig.gsub(/\+/,'\\+').gsub(/-/,'\\-')
+        end
+    end
+
+end
