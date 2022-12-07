11b3cebd77eccf56edcfe388d711139d5ff7a5aa
jmederly <jan.mederly@evolveum.com>
2022-12-07 18:55:04 +0100

    Remove label from <a>

--- evolveum-jekyll-theme/assets/js/evolveum/search.js
+++ evolveum-jekyll-theme/assets/js/evolveum/search.js
@@ -228,9 +228,9 @@
                     showItems.push(`<div><span class="trigger-details" style="display: inline-block;width: 430px" data-toggle="tooltip" data-placement="left" 
                     data-html="true" title='<span class="tooltip-preview"><p>Last modification date: ${date.toLocaleDateString('en-GB', { timeZone: 'UTC' })}</p>
                     <p>Upkeep status: ${upkeepStatus} <i id="upkeep${upkeepStatus}" class="fa fa-circle"></i>
-                    </p><p>Likes: ${upvotes}</p><p>Author: ${author}</p></span>'><a href="${data.hits.hits[i]._source.url}" 
-                    id="${data.hits.hits[i]._id}site"><li class="list-group-item border-0 search-list-item"><i class="fas fa-align-left"></i>
-                    <span class="font1">&nbsp;${data.hits.hits[i].highlight.title}<span id="label${data.hits.hits[i]._source.type}" class="typeLabel">${data.hits.hits[i]._source.type.toUpperCase()}</span><br></span><span class="font2">${preview}</span></li></a></span>
+                    </p><p>Likes: ${upvotes}</p><p>Author: ${author}</p></span>'><div id="${data.hits.hits[i]._id}site"><a href="${data.hits.hits[i]._source.url}" 
+                    ><li class="list-group-item border-0 search-list-item"><i class="fas fa-align-left"></i>
+                    <span class="font1">&nbsp;${data.hits.hits[i].highlight.title}</a><span id="label${data.hits.hits[i]._source.type}" class="typeLabel">${data.hits.hits[i]._source.type.toUpperCase()}</span><a><br></span><span class="font2">${preview}</span></li></a></div></span>
                     <span class="vote" id="${data.hits.hits[i]._id}up"><i class="fas fa-thumbs-up"></i></span></div>`);
                 }
 
