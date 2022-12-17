1c186821681e949cb93b15603c9acc2fa6d1a98c
jmederly <jan.mederly@evolveum.com>
2022-12-17 20:36:57 +0100

    Another fix

--- evolveum-jekyll-theme/assets/js/evolveum/search.js
+++ evolveum-jekyll-theme/assets/js/evolveum/search.js
@@ -327,6 +327,7 @@
                     case 38: // Up arrow    
                         // Remove the highlighting from the previous element
                         listItems[currentLI].classList.remove("highlightSearch");
+                        listItems[currentLI].blur()
 
                         currentLI = currentLI > 0 ? --currentLI : 0; // Decrease the counter      
                         listItems[currentLI].classList.add("highlightSearch"); // Highlight the new element
@@ -335,6 +336,7 @@
                     case 40: // Down arrow
                         // Remove the highlighting from the previous element
                         listItems[currentLI].classList.remove("highlightSearch");
+                        listItems[currentLI].blur()
 
                         currentLI = currentLI < listItems.length - 1 ? ++currentLI : listItems.length - 1; // Increase counter 
                         listItems[currentLI].classList.add("highlightSearch"); // Highlight the new element
