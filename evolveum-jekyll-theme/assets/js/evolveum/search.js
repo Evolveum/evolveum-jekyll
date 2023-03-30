42a5f2c428894a704a75336d7d7680be4d284cd4
jmederly <jan.mederly@evolveum.com>
2023-03-31 00:17:30 +0200

    Another fix of search.js

--- evolveum-jekyll-theme/assets/js/evolveum/search.js
+++ evolveum-jekyll-theme/assets/js/evolveum/search.js
@@ -44,7 +44,7 @@
 
     function OSrequest(method, url, query, async, callback) {
         if (method == "GET" && query != undefined) {
-            url = url + "?source_content_type=application/json&source=" + encodeURIComponent(JSON.stringify(query).replace(/\s/g, "").replace(/\\n/g, "").replace(/elseif/g, "else if"))
+            url = url + "?source_content_type=application/json&source=" + encodeURIComponent(JSON.stringify(query).replace(/\s/g, "").replace(/\\n/g, "").replace(/elseif/g, "else if").replace(/;/g, "; "))
             console.log(url)
             query = undefined
         }
