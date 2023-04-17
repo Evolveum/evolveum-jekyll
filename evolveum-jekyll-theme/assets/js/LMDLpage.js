c6fdc4eec5f35c91c951ae048c4d6afb2b35b8b9
jmederly <jan.mederly@evolveum.com>
2023-04-17 19:53:40 +0200

    Test commit no.1

--- evolveum-jekyll-theme/assets/js/LMDLpage.js
+++ evolveum-jekyll-theme/assets/js/LMDLpage.js
@@ -227,7 +227,7 @@
         <th scope="row"><a href="${data.hits.hits[i]._source.url}" class="LMDLelementTooltip" data-toggle="tooltip" data-html="true" data-original-title='<span>Upkeep status:&nbsp;<i id="upkeep${upkeepStatus}" class="fa fa-circle LMDLupkeep${upkeepStatus}"></i>${unknownStatus}</span>'>${title}</a>&nbsp;<a class="LMDLtitleGithubLink" href="https://github.com/Evolveum/docs/commits/master/${data.hits.hits[i]._source.gitUrl}">history&nbsp;<i class="fab fa-github"></i></a><i data-toggle="tooltip" title="${contentStatus}" class="${contentTriangleClass}"></th>
         <td class="LMDLcategory${contentType} LMDLcategory">${contentType.toUpperCase()}</td>
         <td class="tableCentered LMDLimpact${impactOfChange} LMDLimpact">${impactOfChange.toUpperCase()}</td>
-        <td class="tableCentered">${author}</td>
+        <td class="tableCentered">TEST1</td>
         <td class="tableCentered">${date.toLocaleDateString('en-GB', { timeZone: 'UTC' })}</td>
         <td>${commitMessage}</td>
         </tr>`);
