687316ddc20aedce7e87ddb6df37e7e29f5192cd
jmederly <jan.mederly@evolveum.com>
2023-09-13 00:08:48 +0200

    Another try

--- evolveum-jekyll-plugin/lib/evolveum/jekyll-nav-plugin.rb
+++ evolveum-jekyll-plugin/lib/evolveum/jekyll-nav-plugin.rb
@@ -495,8 +495,8 @@ module Evolveum
 
         # NOTE: this may not work well until we have all labels generated correctly
         def presentableSubnodes(params = {})
-            puts(subnodes)
-            puts(subnodes.select{ |node| node.presentable?(params) })
+            puts("test" + subnodes)
+            puts("test2" + subnodes.select{ |node| node.presentable?(params) })
             subnodes.select{ |node| node.presentable?(params) }.sort{ |a,b| sortCompare(a,b) }
         end
 
