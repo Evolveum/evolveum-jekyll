28452973a80559a9ffd85c12989755948b584770
jmederly <jan.mederly@gmail.com>
2023-10-05 20:26:08 +0200

    Fix reference-versioning.js

--- evolveum-jekyll-theme/assets/js/evolveum/reference-versioning.js
+++ evolveum-jekyll-theme/assets/js/evolveum/reference-versioning.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () {
     let url = window.location.href
     let urlSubstrings = url.split("/")
-    let version = urlSubstrings[5]
+    let version = urlSubstrings[5].charAt(0).toUpperCase() + urlSubstrings[5].slice(1) //Maybe problem when number is first?
 
     if (!url.includes("/midpoint/reference")) {
         $("#select-version").style.display = "none";
