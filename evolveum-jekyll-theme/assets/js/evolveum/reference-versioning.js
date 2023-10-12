41286ae2e836a89265dfdd224b0eea7b5f4abcb9
jmederly <jan.mederly@gmail.com>
2023-10-12 15:54:40 +0200

    Try to fix color of branch labels

--- evolveum-jekyll-theme/assets/js/evolveum/reference-versioning.js
+++ evolveum-jekyll-theme/assets/js/evolveum/reference-versioning.js
@@ -1,18 +1,34 @@
 var DOCSBRANCHESCOLORS = new Map();
 
+function decToHex(dec) {
+    return dec.toString(16);
+}
+
+function padToTwo(str) {
+    return str.padStart(2, '0');
+}
+
+function rgbToHex(r, g, b) {
+    const hexR = padToTwo(decToHex(r));
+    const hexG = padToTwo(decToHex(g));
+    const hexB = padToTwo(decToHex(b));
+
+    return `#${hexR}${hexG}${hexB}`;
+}
+
 window.addEventListener('load', function() {
     console.log($("#select-version-picker-search"))
     let options = document.getElementById('select-version-picker-search').options
     console.log("opts" + options)
-    let start = 40
-    let end = 215
+    let start = 60
+    let end = 210
     console.log(options.length)
     let step = (end - start) / options.length
     console.log("ADB" + options)
     for (let o = 0; o < options.length; o++) {
         console.log(start + (o*step))
         console.log(o)
-        DOCSBRANCHESCOLORS.set(options[o].value, start + (o*step))
+        DOCSBRANCHESCOLORS.set(options[o].value, rgbToHex(start + (o*step),start + (o*step),start + (o*step) + 20))
     }
     let url = window.location.href
     let urlSubstrings = url.split("/")
