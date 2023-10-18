37959f2d4f210b094d62d791c2df613d8fc06141
jmederly <jan.mederly@gmail.com>
2023-10-18 19:24:00 +0200

    Fix reference-versioning.js

--- evolveum-jekyll-theme/assets/js/evolveum/reference-versioning.js
+++ evolveum-jekyll-theme/assets/js/evolveum/reference-versioning.js
@@ -42,7 +42,7 @@ window.addEventListener('load', function() {
     let urlSubstrings = url.split("/")
     let version = ""
     if (urlSubstrings.length > 6) {
-        version = urlSubstrings[5].charAt(0).toUpperCase() + urlSubstrings[5].slice(1) //Maybe problem when number is first?
+        version = DOCSBRANCHDISPLAYNAMES[urlSubstrings[5]] //Maybe problem when number is first?
     }
 
     if (!url.includes("/midpoint/reference")) {
@@ -53,12 +53,9 @@ window.addEventListener('load', function() {
 
     $('#select-version-picker').on('changed.bs.select', function(e, clickedIndex, isSelected, previousValue) {
         let newVersion = $(this).find('option').eq(clickedIndex).text();
-        console.log(newVersion)
-        let versionEdited = version.charAt(0).toLowerCase() + version.slice(1)
-        console.log(versionEdited)
-        let newVersionEdited = newVersion.charAt(0).toLowerCase() + newVersion.slice(1)
+        let newVersionEdited = DOCSBRANCHDISPLAYNAMES[newVersion]
         console.log(newVersionEdited)
         console.log(url)
-        window.location = url.replace(versionEdited, newVersionEdited)
+        window.location = url.replace(version, newVersionEdited)
     });
 });
