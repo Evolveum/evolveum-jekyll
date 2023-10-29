0617443d7768b89e4eda2d953be7b3ed4600541e
jmederly <jan.mederly@gmail.com>
2023-10-29 21:21:42 +0100

    Update site review

--- evolveum-jekyll-theme/assets/js/evolveum/site-review.js
+++ evolveum-jekyll-theme/assets/js/evolveum/site-review.js
@@ -1,4 +1,55 @@
 $(".siteReviewThumb").on("click", function() {
     $(this).toggleClass('on');
-    $("thanksFeedback").toggleClass('on');
+    $(".thanksFeedback").toggleClass('on');
 });
+
+$('#noSiteReviewThumb').popover({
+    html: true,
+    sanitize: false,
+    container: '#search-modal',
+    title: "Report a problem",
+    content: `<div>
+                <div class="form-group">
+                    <label for="searchReportAProblemSelect">Select the type of problem</label>
+                    <select id="searchReportAProblemSelect" data-style="btn-light btn-sm btnSearchSelectReport" title="Type of a problem" data-width="auto">
+                        <option class="input-sm searchReportAProblemOption">Visual bug</option>
+                        <option class="input-sm searchReportAProblemOption">Functional bug</option>
+                        <option class="input-sm searchReportAProblemOption">Problem with results</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="searchReportTextArea">Details of the problem</label>
+                    <textarea class="form-control" id="searchReportTextArea" rows="5"></textarea>
+                </div>
+                <span>
+                    <button type="button" class="btn btn-primary" id="reportDocsProblemPopoverClose">Close</button>
+                    <button type="button" class="btn btn-primary" id="reportDocsProblemPopoverSend">Send message</button>
+                </span>
+            </div>`
+});
+
+$('#noSiteReviewThumb').on('inserted.bs.popover', function() {
+    $('#reportDocsProblemPopoverClose').click(function() {
+        $('#reportDocsProblemPopover').popover('hide');
+    });
+
+    //$('#searchReportAProblemSelect').selectpicker();
+
+    // $('#reportSearchProblemPopoverSend').click(function() {
+    //     let searchProblemSelected = $(".searchReportAProblemOption.selected")
+    //     let searchProblemCategory = "Not defined"
+    //     if (searchProblemSelected[0] != undefined) {
+    //         searchProblemCategory = searchProblemSelected[0].childNodes[0].textContent
+    //     }
+
+    //     let reportSearchQuery = {
+    //         category: searchProblemCategory,
+    //         details: $("#searchReportTextArea").val(),
+    //         query: document.getElementById('searchbar').value,
+    //         width: $(document).width(),
+    //         height: $(document).height()
+    //     }
+    //     OSrequest("POST", "https://docstest.evolveum.com/webhooks/docsreport", reportSearchQuery, true)
+    //     $('#reportSearchProblemPopover').popover('hide');
+    // });
+})
