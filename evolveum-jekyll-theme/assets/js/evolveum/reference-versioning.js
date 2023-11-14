cf54aab7fcb2bdb0a2f20f0b57eeee76aeddd5e7
jmederly <jan.mederly@gmail.com>
2023-11-14 14:06:07 +0100

    Update page.html

--- evolveum-jekyll-theme/assets/js/evolveum/reference-versioning.js
+++ evolveum-jekyll-theme/assets/js/evolveum/reference-versioning.js
@@ -32,12 +32,14 @@ window.addEventListener('load', function() {
         console.log(o)
         console.log(options[o].dataset['tokens'])
         console.log(options[o])
-        if (options[0].dataset['default'] != undefined && options[0].dataset['default'] != null && options[0].dataset['default'] == "default") {
-            console.log(options[0].dataset['default'])
+        let branch = options[o].dataset['tokens']
+        if (options[0].dataset['tokens'].includes(" default")) {
+            console.log(options[0].dataset['tokens'])
+            branch = options[o].dataset['tokens'].replace(" default", "")
             DEFAULTDOCSBRANCH = options[o].dataset['tokens']
         }
-        DOCSBRANCHDISPLAYNAMES[options[o].value] = options[o].dataset['tokens']
-        DOCSBRANCHDISPLAYNAMES[options[o].dataset['tokens']] = options[o].value
+        DOCSBRANCHDISPLAYNAMES[options[o].value] = branch
+        DOCSBRANCHDISPLAYNAMES[branch] = options[o].value
         DOCSBRANCHESCOLORS.set(options[o].value, rgbToHex(start + (o*step),start + (o*step),start + (o*step) + 35))
     }
     let url = window.location.href
