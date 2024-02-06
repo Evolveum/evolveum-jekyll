1c3e07a5d70dcb46f37ddf6970e48b2d260b7f55
jmederly <jan.mederly@gmail.com>
2024-02-06 18:37:07 +0100

    Update initialize-tables.js

--- evolveum-jekyll-theme/assets/js/evolveum/initialize-tables.js
+++ evolveum-jekyll-theme/assets/js/evolveum/initialize-tables.js
@@ -1,3 +1,13 @@
 window.addEventListener('load', function() {
-    $(".sortableTable").DataTable();
+    $(".sortableTableSmallDefaultLength").DataTable({
+        "lengthMenu": [ 10, 25, 50, 75, 100 ]
+    });
+
+    $(".sortableTableMediumDefaultLength").DataTable({
+        "lengthMenu": [ 35, 10, 25, 50, 75, 100 ]
+    });
+
+    $(".sortableTableLargeDefaultLength").DataTable({
+        "lengthMenu": [ 100, 10, 25, 50, 75 ]
+    });
 })
