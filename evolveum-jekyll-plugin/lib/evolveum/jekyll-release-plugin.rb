bfd25279acc3ec7eab6e26f922eecb350fe9d48d
jmederly <jan.mederly@gmail.com>
2024-02-08 00:25:30 +0100

    Add release plugin

---
+++ evolveum-jekyll-plugin/lib/evolveum/jekyll-release-plugin.rb
@@ -0,0 +1,26 @@
+
+require 'yaml'
+
+def installReleaseNotes()
+  versions = readVersions()
+  versions.each do |ver|
+    `cd /docs/midpoint/release/ && mkdir #{ver} && cd #{ver} && wget https://raw.githubusercontent.com/Evolveum/midpoint/v#{ver}/release-notes.adoc
+    && mv release-notes.adoc index.adoc && wget https://raw.githubusercontent.com/Evolveum/midpoint/v#{ver}/install-dist.adoc && mv install-dist.adoc install.adoc`
+  end
+end
+
+def readVersions()
+  verObject = YAML.load_file('/docs/_data/midpoint-versions.yml')
+  versionsNumbers = []
+  verObject.each do |ver|
+    if (ver.version.to_d >= 4.8 )
+      versionsNumbers.push(ver.version.to_d)
+    end
+  end
+  return(versionsNumbers)
+end
+
+Jekyll::Hooks.register :site, :after_init do |site|
+  puts "=========[ EVOLVEUM RELEASE NOTES INSTALL ]============== after_init"
+  installReleaseNotes()
+end
