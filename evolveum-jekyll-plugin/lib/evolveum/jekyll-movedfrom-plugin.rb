f9e2a21ff09d73c87c343ab434bc4473ff67638a
jmederly <jan.mederly@gmail.com>
2024-02-15 20:33:38 +0100

    Fix typo

--- evolveum-jekyll-plugin/lib/evolveum/jekyll-movedfrom-plugin.rb
+++ evolveum-jekyll-plugin/lib/evolveum/jekyll-movedfrom-plugin.rb
@@ -1,5 +1,5 @@
 Jekyll::Hooks.register :pages, :post_init do |page|
-  if (page.data['moved-from'] != nil) {
+  if (page.data['moved-from'] != nil)
     File.write('/testMovedFrom', page.data['moved-from'], mode: 'a')
-  }
+  end
 end
