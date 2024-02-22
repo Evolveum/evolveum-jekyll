7ee429357534110df201d17b650815e14be9cc55
jmederly <jan.mederly@gmail.com>
2024-02-22 01:56:01 +0100

    Update release plugin

--- evolveum-jekyll-plugin/lib/evolveum/jekyll-release-plugin.rb
+++ evolveum-jekyll-plugin/lib/evolveum/jekyll-release-plugin.rb
@@ -2,21 +2,39 @@
 require 'yaml'
 
 def installReleaseNotes()
-  versions = readReleaseVersions()
-  versions.each do |ver|
-    `rm -rf /docs/midpoint/release/#{ver} && cd /docs/midpoint/release/ && mkdir #{ver} && cd #{ver} && wget -q https://raw.githubusercontent.com/Evolveum/midpoint/v#{ver}/release-notes.adoc && mv release-notes.adoc index.adoc && wget -q https://raw.githubusercontent.com/Evolveum/midpoint/v#{ver}/install-dist.adoc && mv install-dist.adoc install.adoc`
+  returnedVerArr = readReleaseVersions()
+  versions = returnedVerArr[0]
+  versionsReleaseBranches = returnedVerArr[1]
+  versions.each_with_index do |ver, index|
+    if Dir["/docs/midpoint/release/#{ver}"].empty?
+      `cd /docs/midpoint/release/ && mkdir #{ver}`
+    end
+
+    if (!File.exist?("/docs/midpoint/release/#{ver}/index.adoc"))
+      `cd /docs/midpoint/release/#{ver}/ && wget -q https://raw.githubusercontent.com/Evolveum/midpoint/#{versionsReleaseBranches[index]}/release-notes.adoc && mv release-notes.adoc index.adoc`
+    end
+
+    if (!File.exist?("/docs/midpoint/release/#{ver}/install.adoc"))
+      `cd /docs/midpoint/release/#{ver}/ && wget -q https://raw.githubusercontent.com/Evolveum/midpoint/#{versionsReleaseBranches[index]}/install-dist.adoc && mv install-dist.adoc install.adoc`
+    end
   end
 end
 
 def readReleaseVersions()
   verObject = YAML.load_file('/docs/_data/midpoint-versions.yml')
   versionsNumbers = []
+  versionBranches = []
   verObject.each do |ver|
-    if ((!ver.key?("legacyDocs") || ver["legacyDocs"] != true ) && ver["status"] != "planned" && ver["status"] != "development")
+    if ((!ver.key?("legacyDocs") || ver["legacyDocs"] != true ))
       versionsNumbers.push(ver["version"].to_f)
+      if (ver["docsReleaseBranch"] != nil)
+        versionBranches.push(ver["docsReleaseBranch"])
+      else
+        versionBranches.push("master")
+
     end
   end
-  return(versionsNumbers)
+  return(versionsNumbers, versionBranches)
 end
 
 Jekyll::Hooks.register :site, :after_init do |site|
