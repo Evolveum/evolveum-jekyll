928b957374a58a675607a3f66c849f6af049ab2c
jmederly <jan.mederly@gmail.com>
2024-02-23 02:26:31 +0100

    Update release plugin

--- evolveum-jekyll-plugin/lib/evolveum/jekyll-release-plugin.rb
+++ evolveum-jekyll-plugin/lib/evolveum/jekyll-release-plugin.rb
@@ -5,17 +5,26 @@ def installReleaseNotes()
   returnedVerArr = readReleaseVersions()
   versions = returnedVerArr[0]
   versionsReleaseBranches = returnedVerArr[1]
+  docsBranches = returnedVerArr[2]
   versions.each_with_index do |ver, index|
     if Dir["/docs/midpoint/release/#{ver}"].empty?
       `cd /docs/midpoint/release/ && mkdir #{ver}`
     end
 
     if (!File.exist?("/docs/midpoint/release/#{ver}/index.adoc"))
-      `cd /docs/midpoint/release/#{ver}/ && wget -q https://raw.githubusercontent.com/Evolveum/midpoint/#{versionsReleaseBranches[index]}/release-notes-#{ver}.adoc && mv release-notes-#{ver}.adoc index.adoc`
+      if (docsBranches._include?(versionsReleaseBranches[index]))
+        `ln -s /mp-#{versionsReleaseBranches[index].gsub("docs/","")}/release-notes-#{ver}.adoc /docs/midpoint/release/#{ver}/index.adoc`
+      else
+        `cd /docs/midpoint/release/#{ver}/ && wget -q https://raw.githubusercontent.com/Evolveum/midpoint/#{versionsReleaseBranches[index]}/release-notes-#{ver}.adoc && mv release-notes-#{ver}.adoc index.adoc`
+      end
     end
 
     if (!File.exist?("/docs/midpoint/release/#{ver}/install.adoc"))
-      `cd /docs/midpoint/release/#{ver}/ && wget -q https://raw.githubusercontent.com/Evolveum/midpoint/#{versionsReleaseBranches[index]}/install-dist-#{ver}.adoc && mv install-dist-#{ver}.adoc install.adoc`
+      if (docsBranches._include?(versionsReleaseBranches[index]))
+        `ln -s /mp-#{versionsReleaseBranches[index].gsub("docs/","")}/install-dist-#{ver}.adoc /docs/midpoint/release/#{ver}/install.adoc`
+      else
+        `cd /docs/midpoint/release/#{ver}/ && wget -q https://raw.githubusercontent.com/Evolveum/midpoint/#{versionsReleaseBranches[index]}/install-dist-#{ver}.adoc && mv install-dist-#{ver}.adoc install.adoc`
+      end
     end
   end
 end
@@ -24,7 +33,11 @@ def readReleaseVersions()
   verObject = YAML.load_file('/docs/_data/midpoint-versions.yml')
   versionsNumbers = []
   versionBranches = []
+  docsBranches = []
   verObject.each do |ver|
+    if ver['docsBranch'] != nil && ver['docsDisplayBranch']
+      docsBranches.push(ver['docsBranch'])
+    end
     if ((!ver.key?("legacyDocs") || ver["legacyDocs"] != true ))
       versionsNumbers.push(ver["version"].to_f)
       if (ver["docsReleaseBranch"] != nil)
@@ -34,7 +47,7 @@ def readReleaseVersions()
       end
     end
   end
-  return versionsNumbers, versionBranches
+  return versionsNumbers, versionBranches, docsBranches
 end
 
 Jekyll::Hooks.register :site, :after_init do |site|
