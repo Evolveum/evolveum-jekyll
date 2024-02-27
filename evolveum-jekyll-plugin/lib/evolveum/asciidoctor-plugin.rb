b3ee4eca9e85889c96df9bf872e984c53d1a8f2d
jmederly <jan.mederly@gmail.com>
2024-02-27 01:58:21 +0100

    Remove backslash

--- evolveum-jekyll-plugin/lib/evolveum/asciidoctor-plugin.rb
+++ evolveum-jekyll-plugin/lib/evolveum/asciidoctor-plugin.rb
@@ -406,7 +406,7 @@ module Evolveum
                 #samplesHtml = samplesDoc.convert
                 fileExt = File.extname(target)[1..-1]
                 if (fileExt == "csv")
-                    samplesHtml = `asciidoctor -e - <<EOF[%header,format=#{fileExt}]\n\|===\ninclude::#{samplesDir}/#{target}[]\n|===\nEOF` #maybe prevent eof prom appering in a file
+                    samplesHtml = `asciidoctor -e - <<EOF[%header,format=#{fileExt}]\n|===\ninclude::#{samplesDir}/#{target}[]\n|===\nEOF` #maybe prevent eof prom appering in a file
                 else
                     samplesHtml = `asciidoctor -e - <<EOF[source,#{fileExt}]\n----\n#{File.read("#{samplesDir}/#{target}")}\n----\nEOF`
                 end
