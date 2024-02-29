789fe0b88908c3a3d18741180b3cb18da3d6feac
jmederly <jan.mederly@gmail.com>
2024-02-29 21:54:07 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DOCSPATH="../"
+DOCSPATH="$HOME"
 DOCSDIR="docs"
-MPPATH="../"
+MPPATH="$HOME"
 MPPREFIX="mp-"
-SAMPLESPATH="../"
+SAMPLESPATH="$HOME"
 SAMPLESDIR="mp-samples"
 
 for arg in "$@"; do
@@ -16,6 +16,7 @@ for arg in "$@"; do
     '--mp-prefix')     set -- "$@" '-d'   ;;
     '--samples-path')     set -- "$@" '-e'   ;;
     '--samples-dir')     set -- "$@" '-f'   ;;
+    '--local')     set -- "$@" '-l'   ;;
     *)          set -- "$@" "$arg" ;;
   esac
 done
@@ -29,6 +30,7 @@ do
         d) MPPREFIX=${OPTARG}; CHANGED=true ;;
         e) SAMPLESPATH=${OPTARG}; CHANGED=true ;;
         f) SAMPLESDIR=${OPTARG}; CHANGED=true ;;
+        l) LOCAL=true;;
         h | *)
           HELP=true
           echo "script usage:"
@@ -38,7 +40,8 @@ do
           echo "--mp-path       custom path to a midpoint directories (for documentation versioning). Default is ../"
           echo "--mp-prefix     custom prefix of midpoint directories. Name of a given midpoint directory consist from prefix + name of the midpoint branch without \"docs/\". Default is \"mp-\""
           echo "--samples-path	custom path to a midpoint samples directory. Default is ../"
-          echo "--samples-dir	custom name of a midpoint samples directory. Default is mp-samples"
+          echo "--samples-dir	  custom name of a midpoint samples directory. Default is mp-samples"
+          echo "--local         create new _configLocal.yml file in home directory that contains user's home paths. It can be used when building site so it helps with merging local branch to other branches."
           ;; 
     esac
 done
@@ -46,14 +49,24 @@ done
 if [ -z $HELP ]
 then
 
-	if [ $CHANGED ]
-	then
-		sed -i "s#.*docsPath:.*#  docsPath: $DOCSPATH#" evolveum-jekyll-theme/_config.yml
-		sed -i "s#.*docsDirName:.*#  docsDirName: $DOCSDIR#" evolveum-jekyll-theme/_config.yml
-		sed -i "s#.*midpointVersionsPath:.*#  midpointVersionsPath: $MPPATH#" evolveum-jekyll-theme/_config.yml
-		sed -i "s#.*midpointVersionsPrefix:.*#  midpointVersionsPrefix: $MPPREFIX#" evolveum-jekyll-theme/_config.yml
-		sed -i "s#.*midpointSamplesPath:.*#  midpointSamplesPath: $SAMPLESPATH#" evolveum-jekyll-theme/_config.yml
-		sed -i "s#.*midpointSamplesDir:.*#  midpointSamplesDir: $SAMPLESDIR#" evolveum-jekyll-theme/_config.yml
+  if [ $LOCAL ]
+  then
+
+    cat <<EOF > $HOME/_configLocal.yml
+docs:
+  docsPath: $DOCSPATH
+  midpointVersionsPath: $MPPATH
+  midpointSamplesPath: $SAMPLESPATH
+EOF
+
+    elif [ $CHANGED ]
+  then
+    sed -i "s#.*docsPath:.*#  docsPath: $DOCSPATH#" evolveum-jekyll-theme/_config.yml
+    sed -i "s#.*docsDirName:.*#  docsDirName: $DOCSDIR#" evolveum-jekyll-theme/_config.yml
+    sed -i "s#.*midpointVersionsPath:.*#  midpointVersionsPath: $MPPATH#" evolveum-jekyll-theme/_config.yml
+    sed -i "s#.*midpointVersionsPrefix:.*#  midpointVersionsPrefix: $MPPREFIX#" evolveum-jekyll-theme/_config.yml
+    sed -i "s#.*midpointSamplesPath:.*#  midpointSamplesPath: $SAMPLESPATH#" evolveum-jekyll-theme/_config.yml
+    sed -i "s#.*midpointSamplesDir:.*#  midpointSamplesDir: $SAMPLESDIR#" evolveum-jekyll-theme/_config.yml
 	fi
 
 	VERSION=0.1.0
