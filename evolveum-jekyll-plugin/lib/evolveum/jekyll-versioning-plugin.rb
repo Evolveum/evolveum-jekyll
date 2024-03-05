77f9f0481020ca9ae1d7c76b25b96d093348e745
jmederly <jan.mederly@gmail.com>
2024-03-05 02:42:16 +0100

    Fix versioning plugin

--- evolveum-jekyll-plugin/lib/evolveum/jekyll-versioning-plugin.rb
+++ evolveum-jekyll-plugin/lib/evolveum/jekyll-versioning-plugin.rb
@@ -35,47 +35,46 @@ module VersionReader
   def self.get_config_value(key)
     @config[key]
   end
+end
 
-  def installVersions(site)
-    docsDir = site.config['docs']['docsPath'] + site.config['docs']['docsDirName']
-    mpPreDir = site.config['docs']['midpointVersionsPath'] + site.config['docs']['midpointVersionsPrefix']
-    load_config()
-    #arr = readVersions(docsDir)
-    #versions = arr[0]
-    #displayVersions = arr[1]
-    #defaultBranch = arr[2]
-    system("rm -rf #{docsDir}/midpoint/reference/*")
-    system("cp /mnt/index.html #{docsDir}/midpoint/reference/")
-    negativeAssert = "?!(?:"
-    @config['versions'].each do |version|
-      versionWithoutDocs = version.gsub("docs/","")
-      negativeAssert << "#{versionWithoutDocs}|"
-      puts("?!#{versionWithoutDocs}|")
-    end
-    negativeAssert.chop!
-    negativeAssert << ")"
-    puts(negativeAssert)
-    system("find #{docsDir} -mindepth 1 -not -path '*/[@.]*' -type f -exec perl -pi -e 's/xref:\\/midpoint\\/reference\\/(#{negativeAssert})/xrefv:\\/midpoint\\/reference\\/#{@config['defaultBranch'].gsub("docs/","")}\\//g' {} +")
-    system("find #{docsDir} -mindepth 1 -not -path '*/[@.]*' -type f -exec perl -pi -e 's/midpoint\\/reference\\/(#{negativeAssert})/midpoint\\/reference\\/#{@config['defaultBranch'].gsub("docs/","")}\\//g' {} +")
-
-    @config['versions'].each_with_index do |version, index|
-      versionWithoutDocs = version.gsub("docs/","")
-      if Dir["#{mpPreDir}#{versionWithoutDocs}"].empty?
-        system("cd #{site.config['docs']['midpointVersionsPath']} && git clone -b #{version} https://github.com/janmederly/testversioning #{site.config['docs']['midpointVersionsPrefix']}#{versionWithoutDocs} && rm #{mpPreDir}#{versionWithoutDocs}/docs/LICENSE") #maybe
-      end
-      if version != @config['defaultBranch']
-       system("grep -rl :page-alias: #{mpPreDir}#{versionWithoutDocs}/docs/ | xargs -P 4 sed -i '/:page-alias:/d' 2> /dev/null || true")
-      end
-      system("cd #{site.config['docs']['midpointVersionsPath']} && ln -s \"$PWD\"/#{site.config['docs']['midpointVersionsPrefix']}#{versionWithoutDocs}/docs/ \"$PWD\"/#{site.config['docs']['docsDirName']}/midpoint/reference/#{versionWithoutDocs}")
-      system("sed -i 's/:page-nav-title: Configuration Reference/:page-nav-title: \"#{@config["displayVersions"][index]}\"/g' #{mpPreDir}#{versionWithoutDocs}/docs/index.adoc")
-      system("find #{mpPreDir}#{versionWithoutDocs}/docs -type f -exec perl -pi -e 's/xref:\\/midpoint\\/reference\\/(#{negativeAssert})/xrefv:\\/midpoint\\/reference\\/#{versionWithoutDocs}\\//g' {} +")
-      system("find #{mpPreDir}#{versionWithoutDocs}/docs -type f -exec perl -pi -e 's/midpoint\\/reference\\/(#{negativeAssert})/midpoint\\/reference\\/#{versionWithoutDocs}\\//g' {} +")
-    end
+def installVersions(site)
+  docsDir = site.config['docs']['docsPath'] + site.config['docs']['docsDirName']
+  mpPreDir = site.config['docs']['midpointVersionsPath'] + site.config['docs']['midpointVersionsPrefix']
+  VersionReader.load_config()
+  #arr = readVersions(docsDir)
+  #versions = arr[0]
+  #displayVersions = arr[1]
+  #defaultBranch = arr[2]
+  system("rm -rf #{docsDir}/midpoint/reference/*")
+  system("cp /mnt/index.html #{docsDir}/midpoint/reference/")
+  negativeAssert = "?!(?:"
+  VersionReader.get_config_value('filteredVersions').each do |version|
+    versionWithoutDocs = version.gsub("docs/","")
+    negativeAssert << "#{versionWithoutDocs}|"
+    puts("?!#{versionWithoutDocs}|")
   end
+  negativeAssert.chop!
+  negativeAssert << ")"
+  puts(negativeAssert)
+  system("find #{docsDir} -mindepth 1 -not -path '*/[@.]*' -type f -exec perl -pi -e 's/xref:\\/midpoint\\/reference\\/(#{negativeAssert})/xrefv:\\/midpoint\\/reference\\/#{VersionReader.get_config_value('defaultBranch').gsub("docs/","")}\\//g' {} +")
+  system("find #{docsDir} -mindepth 1 -not -path '*/[@.]*' -type f -exec perl -pi -e 's/midpoint\\/reference\\/(#{negativeAssert})/midpoint\\/reference\\/#{VersionReader.get_config_value('defaultBranch').gsub("docs/","")}\\//g' {} +")
 
-  Jekyll::Hooks.register :site, :after_init do |site|
-    puts "=========[ EVOLVEUM VERSIONNING ]============== after_init"
-    installVersions(site)
+  VersionReader.get_config_value('filteredVersions').each_with_index do |version, index|
+    versionWithoutDocs = version.gsub("docs/","")
+    if Dir["#{mpPreDir}#{versionWithoutDocs}"].empty?
+      system("cd #{site.config['docs']['midpointVersionsPath']} && git clone -b #{version} https://github.com/janmederly/testversioning #{site.config['docs']['midpointVersionsPrefix']}#{versionWithoutDocs} && rm #{mpPreDir}#{versionWithoutDocs}/docs/LICENSE") #maybe
+    end
+    if version != VersionReader.get_config_value('defaultBranch')
+     system("grep -rl :page-alias: #{mpPreDir}#{versionWithoutDocs}/docs/ | xargs -P 4 sed -i '/:page-alias:/d' 2> /dev/null || true")
+    end
+    system("cd #{site.config['docs']['midpointVersionsPath']} && ln -s \"$PWD\"/#{site.config['docs']['midpointVersionsPrefix']}#{versionWithoutDocs}/docs/ \"$PWD\"/#{site.config['docs']['docsDirName']}/midpoint/reference/#{versionWithoutDocs}")
+    system("sed -i 's/:page-nav-title: Configuration Reference/:page-nav-title: \"#{VersionReader.get_config_value('filteredDisplayVersions')[index]}\"/g' #{mpPreDir}#{versionWithoutDocs}/docs/index.adoc")
+    system("find #{mpPreDir}#{versionWithoutDocs}/docs -type f -exec perl -pi -e 's/xref:\\/midpoint\\/reference\\/(#{negativeAssert})/xrefv:\\/midpoint\\/reference\\/#{versionWithoutDocs}\\//g' {} +")
+    system("find #{mpPreDir}#{versionWithoutDocs}/docs -type f -exec perl -pi -e 's/midpoint\\/reference\\/(#{negativeAssert})/midpoint\\/reference\\/#{versionWithoutDocs}\\//g' {} +")
   end
+end
 
+Jekyll::Hooks.register :site, :after_init do |site|
+  puts "=========[ EVOLVEUM VERSIONNING ]============== after_init"
+  installVersions(site)
 end
