825c37823277983e8e889b84d0c822e91f62295a
jmederly <jan.mederly@gmail.com>
2024-03-07 03:18:39 +0100

    Update nav plugin

--- evolveum-jekyll-plugin/lib/evolveum/jekyll-nav-plugin.rb
+++ evolveum-jekyll-plugin/lib/evolveum/jekyll-nav-plugin.rb
@@ -303,7 +303,12 @@ module Evolveum
             nav = index_path(page.url)
             nav.url = page.url
             nav.page = page
-            nav.title = page.data['nav-title'] || page.data['title']
+            if (page.data['nav-title'] == "Configuration Reference")
+                nav.title = page.data['midpointBranchDisplayName']
+            else
+                nav.title = page.data['nav-title'] || page.data['title']
+            end
+
             if page.data['visibility'] != nil
                 nav.visibility = page.data['visibility']
             end
