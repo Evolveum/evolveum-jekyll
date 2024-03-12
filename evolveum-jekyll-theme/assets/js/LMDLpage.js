d0a0779f9e4428dc0946b82d91588b0b88cd06b3
jmederly <jan.mederly@gmail.com>
2024-03-12 06:24:41 +0100

    Fix typo

--- evolveum-jekyll-theme/assets/js/LMDLpage.js
+++ evolveum-jekyll-theme/assets/js/LMDLpage.js
@@ -176,7 +176,7 @@
             contentStatusValuesArray = ["obsolete", "deprecated", "experimental", "planned", "outdated"]
             contentStatus = "" // TODO as array
             filtredArray = contentStatusArray.filter(function(element, index) {
-                if (element != undefined && (element || element == "true")) {
+                if (element != undefined && (element == "true")) {
                     contentStatus = contentStatusValuesArray[index]
                     return true;
                 } else {
