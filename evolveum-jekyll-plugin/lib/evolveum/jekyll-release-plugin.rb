5005356cd28cc9b1ee3fd379d1fb953d7afb3432
jmederly <jan.mederly@gmail.com>
2024-03-12 22:24:03 +0100

    Fix typo

--- evolveum-jekyll-plugin/lib/evolveum/jekyll-release-plugin.rb
+++ evolveum-jekyll-plugin/lib/evolveum/jekyll-release-plugin.rb
@@ -30,7 +30,7 @@ def installReleaseNotes(site)
 
     if (!File.exist?("#{docsDir}/midpoint/release/#{ver}/index.adoc"))
       if (docsBranches.include?(versionsReleaseBranches[index]))
-        system("cd #{site.config['docs']['docsPath']} && DOCSPATHVAR=$PWD && cd #{site.config['docs']['midpointVersionsPath']} && ln -s \"$PWD\"/#{site.config['docs']['midpointVersionsPrefix']}#{versionsReleaseBranches[index].gsub("docs/","")}/release-notes.adoc \"$DOCSPATHVAR\"/#{site.config['docs']['docsDirName']}/midpoint/release/#{ver}/index.adoc")
+        system("cd #{site.config['docs']['docsPath']} && DOCSPATHVAR=$PWD && cd #{site.config['docs']['midpointReleasePath']} && ln -s \"$PWD\"/#{site.config['docs']['midpointVersionsPrefix']}#{versionsReleaseBranches[index].gsub("docs/","")}/release-notes.adoc \"$DOCSPATHVAR\"/#{site.config['docs']['docsDirName']}/midpoint/release/#{ver}/index.adoc")
       else
         system("cd #{site.config['docs']['docsPath']} && DOCSPATHVAR=$PWD && cd #{site.config['docs']['midpointReleasePath']} && ln -s \"$PWD\"/#{site.config['docs']['midpointReleaseDir']}/release-notes.adoc \"$DOCSPATHVAR\"/#{site.config['docs']['docsDirName']}/midpoint/release/#{ver}/index.adoc")
       end
@@ -38,7 +38,7 @@ def installReleaseNotes(site)
 
     if (!File.exist?("#{docsDir}/midpoint/release/#{ver}/install.adoc"))
       if (docsBranches.include?(versionsReleaseBranches[index]))
-        system("cd #{site.config['docs']['docsPath']} && DOCSPATHVAR=$PWD && cd #{site.config['docs']['midpointVersionsPath']} && ln -s \"$PWD\"/#{site.config['docs']['midpointVersionsPrefix']}#{versionsReleaseBranches[index].gsub("docs/","")}/install-dist.adoc \"$DOCSPATHVAR\"/#{site.config['docs']['docsDirName']}/midpoint/release/#{ver}/install.adoc")
+        system("cd #{site.config['docs']['docsPath']} && DOCSPATHVAR=$PWD && cd #{site.config['docs']['midpointReleasePath']} && ln -s \"$PWD\"/#{site.config['docs']['midpointVersionsPrefix']}#{versionsReleaseBranches[index].gsub("docs/","")}/install-dist.adoc \"$DOCSPATHVAR\"/#{site.config['docs']['docsDirName']}/midpoint/release/#{ver}/install.adoc")
       else
         system("cd #{site.config['docs']['docsPath']} && DOCSPATHVAR=$PWD && cd #{site.config['docs']['midpointReleasePath']} && ln -s \"$PWD\"/#{site.config['docs']['midpointReleaseDir']}/install-dist.adoc \"$DOCSPATHVAR\"/#{site.config['docs']['docsDirName']}/midpoint/release/#{ver}/install.adoc")
       end
