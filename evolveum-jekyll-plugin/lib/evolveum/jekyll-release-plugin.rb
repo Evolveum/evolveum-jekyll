52cb6409a4cd4918be383488d06ab0b010afefbc
jmederly <jan.mederly@gmail.com>
2024-06-12 08:59:37 +0200

    Update release plugin

--- evolveum-jekyll-plugin/lib/evolveum/jekyll-release-plugin.rb
+++ evolveum-jekyll-plugin/lib/evolveum/jekyll-release-plugin.rb
@@ -2,6 +2,27 @@
 require 'yaml'
 require 'open3'
 
+def readReleaseVersions(docsDir)
+  verObject = YAML.load_file("#{docsDir}/_data/midpoint-versions.yml")
+  versionsNumbers = []
+  versionBranches = []
+  docsBranches = []
+  verObject.each do |ver|
+    if ver['docsBranch'] != nil && ver['docsDisplayBranch']
+      docsBranches.push(ver['docsBranch'])
+    end
+    if ((!ver.key?("legacyDocs") || ver["legacyDocs"] != true ))
+      if (ver["docsReleaseBranch"] != nil)
+        versionBranches.push(ver["docsReleaseBranch"])
+        versionsNumbers.push(ver["version"])
+      end
+    end
+  end
+  docsBranches.push("docs/before-4.8")
+  docsBranches.push("master")
+  return versionsNumbers, versionBranches, docsBranches
+end
+
 def installReleaseNotes(site)
   docsDir = site.config['docs']['docsPath'] + site.config['docs']['docsDirName']
   releaseDir = site.config['docs']['midpointReleasePath'] + site.config['docs']['midpointReleaseDir']
@@ -25,20 +46,20 @@ def installReleaseNotes(site)
         system("cd #{site.config['docs']['midpointReleasePath']} && mkdir -p #{releaseDir}/#{ver}")
       end
 
-      if (!File.exist?("#{releaseDir}/#{ver}/index.adoc"))
+      #if (!File.exist?("#{releaseDir}/#{ver}/index.adoc"))
         system("cd #{releaseDir}/#{ver}/ && wget -q https://raw.githubusercontent.com/Evolveum/midpoint/#{versionsReleaseBranches[index]}/release-notes.adoc && mv release-notes.adoc index.adoc")
-      end
+      #end
 
-      if (!File.exist?("#{releaseDir}/#{ver}/install.adoc"))
+      #if (!File.exist?("#{releaseDir}/#{ver}/install.adoc"))
         system("cd #{releaseDir}/#{ver}/ && wget -q https://raw.githubusercontent.com/Evolveum/midpoint/#{versionsReleaseBranches[index]}/install-dist.adoc && mv install-dist.adoc install.adoc")
-      end
+      #end
     end
 
     if (!File.exist?("#{docsDir}/midpoint/release/#{ver}/index.adoc"))
       if (docsBranches.include?(versionsReleaseBranches[index]))
-        system("ACTPATH=$PWD && cd #{site.config['docs']['docsPath']} && DOCSPATHVAR=$PWD && cd $ACTPATH && cd #{site.config['docs']['midpointVersionsPath']} && ln -sf \"$PWD\"/#{site.config['docs']['midpointVersionsPrefix']}#{versionsReleaseBranches[index].gsub("docs/","")}/release-notes.adoc \"$DOCSPATHVAR\"#{addedSlash}#{site.config['docs']['docsDirName']}/midpoint/release/#{ver}/index.adoc")
+        system("ACTPATH=$PWD && cd #{site.config['docs']['docsPath']} && DOCSPATHVAR=$PWD && cd $ACTPATH && cd #{site.config['docs']['midpointVersionsPath']} && ln -s \"$PWD\"/#{site.config['docs']['midpointVersionsPrefix']}#{versionsReleaseBranches[index].gsub("docs/","")}/release-notes.adoc \"$DOCSPATHVAR\"#{addedSlash}#{site.config['docs']['docsDirName']}/midpoint/release/#{ver}/index.adoc")
       else
-        system("ACTPATH=$PWD && cd #{site.config['docs']['docsPath']} && DOCSPATHVAR=$PWD && cd $ACTPATH && cd #{site.config['docs']['midpointReleasePath']} && ln -sf \"$PWD\"/#{site.config['docs']['midpointReleaseDir']}/#{ver}/index.adoc \"$DOCSPATHVAR\"#{addedSlash}#{site.config['docs']['docsDirName']}/midpoint/release/#{ver}/index.adoc")
+        system("ACTPATH=$PWD && cd #{site.config['docs']['docsPath']} && DOCSPATHVAR=$PWD && cd $ACTPATH && cd #{site.config['docs']['midpointReleasePath']} && ln -s \"$PWD\"/#{site.config['docs']['midpointReleaseDir']}/#{ver}/index.adoc \"$DOCSPATHVAR\"#{addedSlash}#{site.config['docs']['docsDirName']}/midpoint/release/#{ver}/index.adoc")
       end
     else
       output, _ = Open3.capture2("cd #{docsDir}/midpoint/release/#{ver}/ && ls -F index.adoc")
@@ -54,9 +75,9 @@ def installReleaseNotes(site)
 
     if (!File.exist?("#{docsDir}/midpoint/release/#{ver}/install.adoc"))
       if (docsBranches.include?(versionsReleaseBranches[index]))
-        system("ACTPATH=$PWD && cd #{site.config['docs']['docsPath']} && DOCSPATHVAR=$PWD && cd $ACTPATH && cd #{site.config['docs']['midpointVersionsPath']} && ln -sf \"$PWD\"/#{site.config['docs']['midpointVersionsPrefix']}#{versionsReleaseBranches[index].gsub("docs/","")}/install-dist.adoc \"$DOCSPATHVAR\"#{addedSlash}#{site.config['docs']['docsDirName']}/midpoint/release/#{ver}/install.adoc")
+        system("ACTPATH=$PWD && cd #{site.config['docs']['docsPath']} && DOCSPATHVAR=$PWD && cd $ACTPATH && cd #{site.config['docs']['midpointVersionsPath']} && ln -s \"$PWD\"/#{site.config['docs']['midpointVersionsPrefix']}#{versionsReleaseBranches[index].gsub("docs/","")}/install-dist.adoc \"$DOCSPATHVAR\"#{addedSlash}#{site.config['docs']['docsDirName']}/midpoint/release/#{ver}/install.adoc")
       else
-        system("ACTPATH=$PWD && cd #{site.config['docs']['docsPath']} && DOCSPATHVAR=$PWD && cd $ACTPATH && cd #{site.config['docs']['midpointReleasePath']} && ln -sf \"$PWD\"/#{site.config['docs']['midpointReleaseDir']}/#{ver}/install.adoc \"$DOCSPATHVAR\"#{addedSlash}#{site.config['docs']['docsDirName']}/midpoint/release/#{ver}/install.adoc")
+        system("ACTPATH=$PWD && cd #{site.config['docs']['docsPath']} && DOCSPATHVAR=$PWD && cd $ACTPATH && cd #{site.config['docs']['midpointReleasePath']} && ln -s \"$PWD\"/#{site.config['docs']['midpointReleaseDir']}/#{ver}/install.adoc \"$DOCSPATHVAR\"#{addedSlash}#{site.config['docs']['docsDirName']}/midpoint/release/#{ver}/install.adoc")
       end
     else
       output, _ = Open3.capture2("cd #{docsDir}/midpoint/release/#{ver}/ && ls -F install.adoc")
@@ -70,27 +91,6 @@ def installReleaseNotes(site)
       end
     end
   end
-end
-
-def readReleaseVersions(docsDir)
-  verObject = YAML.load_file("#{docsDir}/_data/midpoint-versions.yml")
-  versionsNumbers = []
-  versionBranches = []
-  docsBranches = []
-  verObject.each do |ver|
-    if ver['docsBranch'] != nil && ver['docsDisplayBranch']
-      docsBranches.push(ver['docsBranch'])
-    end
-    if ((!ver.key?("legacyDocs") || ver["legacyDocs"] != true ))
-      if (ver["docsReleaseBranch"] != nil)
-        versionBranches.push(ver["docsReleaseBranch"])
-        versionsNumbers.push(ver["version"])
-      end
-    end
-  end
-  docsBranches.push("docs/before-4.8")
-  docsBranches.push("master")
-  return versionsNumbers, versionBranches, docsBranches
 end
 
 Jekyll::Hooks.register :site, :after_init do |site|
